54f7a9ccb5ced11a8fa953b082d97d8e1ccaa018
dreak603 <dreak603@gmail.com>
2019-11-14 15:15:03 +0800

    revise NavTab and bug fix

--- src/components/NavTabs.vue
+++ src/components/NavTabs.vue
@@ -1,19 +1,47 @@
 <template>
     <b-nav tabs justified>
-        <b-nav-item href="/restaurants" active>
-            首頁
-        </b-nav-item>
-        <b-nav-item href="/restaurants/feeds">
-            最新動態
-        </b-nav-item>
-        <b-nav-item href="/restaurants/top">
-            TOP 10 人氣餐廳
-        </b-nav-item>
-        <b-nav-item href="/users/top">
-            美食達人
+        <b-nav-item
+            v-for="tab in tabs"
+            :key="tab.id"
+            :href="tab.path"
+        >
+            {{tab.title}}
         </b-nav-item>
     </b-nav>
 </template>
+
+<script>
+import uuid from 'uuid'
+
+export default {
+    data() {
+        return {
+            tabs: [
+                {
+                id: uuid(),
+                title: '首頁',
+                path: '/restaurants'
+                },
+                {
+                id: uuid(),
+                title: '最新動態',
+                path: '/restaurants/feeds'
+                },
+                {
+                id: uuid(),
+                title: 'TOP 10 人氣餐廳',
+                path: '/restaurants/top'
+                },
+                {
+                id: uuid(),
+                title: '美食達人',
+                path: '/users/top'
+                }
+            ]
+        }
+    }
+}
+</script>
 
 <style scoped>
 @media screen and (max-width: 425px) {
